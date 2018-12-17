4bd8b1cd487088271bba88649bff4e7854f61033
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-17 22:53:12 +0800

    Create mycommon.cs

---
+++ Assets/3DGamekit/mycommon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets._3DGamekit
+{
+    public static class mycommon
+    {
+        public static int player_id;
+        public static int working_value;
+        public static int luck_value;
+        public static int attack_value;
+        public static int gold_coin;
+        public static int silver_coin;
+        public static Boolean hunting;
+    }
+}
