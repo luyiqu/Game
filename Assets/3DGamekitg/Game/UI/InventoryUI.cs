a6cb7df04bb3a7e59da05a2f8476c992155c53ff
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-17 22:57:10 +0800

    to click the item in the center

---
+++ Assets/3DGamekitg/Game/UI/InventoryUI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Gamekit3D;
+using Gamekit3D.Network;
+
+public class InventoryUI : MonoBehaviour
+{
+
+    public GameObject InventoryCell;
+    public GameObject InventoryGridContent;
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+        InventoryCell.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        PlayerMyController.Instance.EnabledWindowCount++;
+        int capacity = PlayerMyController.Instance.InventoryCapacity;
+        int count = PlayerMyController.Instance.Inventory.Count;
+        foreach (var kv in PlayerMyController.Instance.Inventory)
+        {
+            GameObject cloned = GameObject.Instantiate(InventoryCell);
+            Button button = cloned.GetComponent<Button>();
+            // TODO ... specify icon by item types
+            Sprite icon = GetAllIcons.icons["Sword_2"];
+            //Sprite icon2 = GetAllIcons.icons["Elixir_1"];
+            button.image.sprite = icon;
+            //button.image.sprite = icon2;
+            button.onClick.AddListener(delegate ()
+            {
+                GameObject.Find("ItemImage").GetComponent<Image>().sprite = icon;
+                //GameObject.Find("ItemImage").GetComponent<Image>().sprite = icon2;
+            });
+
+
+            cloned.SetActive(true);
+            cloned.transform.SetParent(InventoryGridContent.transform, false);
+        }
+
+        for (int i = 0; i < capacity - count; i++)
+        {
+            GameObject cloned = GameObject.Instantiate(InventoryCell);
+            cloned.SetActive(true);
+            cloned.transform.SetParent(InventoryGridContent.transform, false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        int cellCount = InventoryGridContent.transform.childCount;
+        foreach (Transform transform in InventoryGridContent.transform)
+        {
+            Destroy(transform.gameObject);
+        }
+        PlayerMyController.Instance.EnabledWindowCount--;
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ExtendBagCapacity(int n)
+    {
+        int cellCount = InventoryGridContent.transform.childCount;
+        for (int i = 0; i < n - cellCount; i++)
+        {
+            GameObject cloned = GameObject.Instantiate(InventoryCell);
+            cloned.SetActive(true);
+            cloned.transform.SetParent(InventoryGridContent.transform, false);
+        }
+    }
+}
