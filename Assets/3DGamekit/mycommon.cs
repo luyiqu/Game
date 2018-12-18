776a559c3b63f4b9c4402683af287a9a2d08bed2
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-18 23:08:43 +0800

    Add treasure attribute

--- Assets/3DGamekit/mycommon.cs
+++ Assets/3DGamekit/mycommon.cs
@@ -7,6 +7,7 @@ namespace Assets._3DGamekit
 {
     public static class mycommon
     {
+        //player attribute
         public static int player_id;
         public static int working_value;
         public static int luck_value;
@@ -14,5 +15,13 @@ namespace Assets._3DGamekit
         public static int gold_coin;
         public static int silver_coin;
         public static Boolean hunting;
+        
+        //treasure attribute
+        public static int treasure_number;
+        public static Dictionary<string, string> name_attribute = new Dictionary<string, string>();  // treasure_name : tool, accessories,equipment
+        public static Dictionary<string, int> name_value = new Dictionary<string, int>();       //  treasure_name : value
+        public static Dictionary<string, string> name_category = new Dictionary<string, string>();    // treasure_name : category
+        public static Dictionary<string, string> name_status = new Dictionary<string, string>();   //  treasure_name : wear,inventory
+
     }
 }
