715aec6496f674739eed7ce67de0f009486d823b
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-26 16:07:05 +0800

    Create CPutoff.cs

---
+++ Assets/3DGamekit/Scripts/FEMessage/CPutoff.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Common
+{
+    [Serializable]
+    public class CPutoff : Message
+    {
+        public CPutoff() : base(Command.C_PUTOFF) { }
+        public string old_treasure;  //穿上的treasure
+        public string treasure_attribute;
+        public int treasure_value;
+
+    }
+}
