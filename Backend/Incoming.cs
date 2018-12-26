82cb9aa47b6a8dbb03043184e49fe1804b612523
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-26 16:11:48 +0800

    Create Incoming.cs

---
+++ Backend/Incoming.cs
@@ -0,0 +1,47 @@
+using Common;
+
+namespace Backend.Network
+{
+    public partial class Incoming
+    {
+        IRegister register;
+        public Incoming(IRegister register)
+        {
+            this.register = register;
+            RegisterAll();
+        }
+
+        private void RegisterAll()
+        {
+            register.Register(Command.C_LOGIN, OnRecvLogin);    //当收到前端发往后端的消息，要执行的脚本
+            register.Register(Command.C_REGISTER, OnRecvRegister);
+            register.Register(Command.C_PLAYER_ENTER, OnRecvPlayerEnter);
+            register.Register(Command.C_PLAYER_MOVE, OnRecvPlayerMove);
+            register.Register(Command.C_PLAYER_JUMP, OnRecvPlayerJump);
+            register.Register(Command.C_PLAYER_ATTACK, OnRecvPlayerAttack);
+            register.Register(Command.C_PLAYER_TAKE, OnRecvPlayerTake);
+            register.Register(Command.C_POSITION_REVISE, OnRecvPositionRevise);
+            register.Register(Command.C_ENEMY_CLOSING, OnRecvEnemyClosing);
+            register.Register(Command.C_DAMAGE, OnRecvDamage);
+            register.Register(Command.C_WEAR, OnRecvWear);
+            register.Register(Command.C_PUTOFF, OnRecvPutoff);
+
+            // DEBUG ..
+            register.Register(Command.C_FIND_PATH, OnRecvFindPath);
+
+        }
+
+
+        static void ClientTipInfo(IChannel channel, string info)
+        {
+            STipInfo tipInfo = new STipInfo();
+            tipInfo.info = info;
+            channel.Send(tipInfo);
+        }
+
+
+
+
+
+    }
+}
