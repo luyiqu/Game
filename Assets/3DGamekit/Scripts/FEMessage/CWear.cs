d2d582c0b4bb22ee6b3c11dbfeffe5a35e22eb4a
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-26 16:06:18 +0800

    Create CWear.cs

---
+++ Assets/3DGamekit/Scripts/FEMessage/CWear.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Common
+{
+    [Serializable]
+    public class CWear : Message
+    {
+        public CWear() : base(Command.C_WEAR) { }
+        public string new_treasure;  //穿上的treasure
+        public string old_treasure;  //如果脱下了原来的，就也要原来的treasure_name,否则为null
+        public string treasure_attribute;
+        public int treasure_value;
+        
+    }
+}
