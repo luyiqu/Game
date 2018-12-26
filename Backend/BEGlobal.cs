0d1dc4b987e053259f238a53cf5d48313fb0ef55
luyiqu <42026475+luyiqu@users.noreply.github.com>
2018-12-26 16:14:26 +0800

    Create BEGlobal.cs

---
+++ Backend/BEGlobal.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class BEGlobal
+    {
+        public static int player_id;
+        public static int working_value;
+        public static int luck_value;
+        public static int attack_value;
+        public static int gold_coin;
+        public static int silver_coin;
+        public static Boolean hunting;
+        
+
+    }
+}
