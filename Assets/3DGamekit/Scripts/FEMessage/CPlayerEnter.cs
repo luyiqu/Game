280b74564f9b9229c9302a5a54ce27647a6cffe5
luyiqu <42026475+luyiqu@users.noreply.github.com>
2019-01-03 22:30:49 +0800

    Create CPlayerEnter.cs

---
+++ Assets/3DGamekit/Scripts/FEMessage/CPlayerEnter.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Common
+{
+    // when a player finishing enter a scene
+    [Serializable]
+    public class CPlayerEnter : Message
+    {
+        public CPlayerEnter() : base(Command.C_PLAYER_ENTER) { }
+        public int player_id;
+    }
+}
